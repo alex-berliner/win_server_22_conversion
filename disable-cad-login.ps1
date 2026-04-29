acbe4dd206a1b894809bef7c72dfceb948580cab
Alex Berliner <alexberliner@gmail.com>
2026-04-29 08:44:04 -0400

    cad comment

--- disable-cad-login.ps1
+++ disable-cad-login.ps1
@@ -1,1 +1,3 @@
+# Disable requirement to press ctrl+alt+del on login screen (can now just press enter or other)
+
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value 1
